eeb6f8545e7b06dfddff88c38296b2de8457619a
Richardo <milkywayw@gmail.com>
2014-06-18 17:22:11 -0500

    Added a simple test class to test a specific board

---
+++ src/milkywayw/games/snake/view/RenderTester.java
@@ -0,0 +1,34 @@
+package milkywayw.games.snake.view;
+
+import java.util.ArrayList;
+
+import milkywayw.utilities.Point;
+
+public class RenderTester
+{
+    private static ArrayList<Point> generateSnake()
+    {
+        ArrayList<Point> snake = new ArrayList<>();
+
+        for (int i = 2; i <= 18; ++i)
+            snake.add(new Point(i, 4));
+
+        for (int j = 4; j <= 20; j++)
+            snake.add(new Point(2, j));
+
+        snake.add(new Point(3, 20));
+
+        return snake;
+    }
+
+    private static Point generateFood()
+    {
+        return new Point(27, 37);
+    }
+
+    public static void main(String[] args)
+    {
+        SnakeRender snakeGame = new SnakeRender(40, 30);
+        snakeGame.update(generateSnake(), generateFood());
+    }
+}
