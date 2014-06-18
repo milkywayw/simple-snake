933d23653af006c26f1bd0ce77897f5a27a2c050
Richardo <milkywayw@gmail.com>
2014-06-18 17:21:35 -0500

    Put more control of the snake-rendering logic here

--- src/milkywayw/games/snake/view/SnakeRender.java
+++ src/milkywayw/games/snake/view/SnakeRender.java
@@ -1,10 +1,10 @@
 package milkywayw.games.snake.view;
 
+import java.awt.Color;
 import java.util.Collection;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 import milkywayw.utilities.Point;
 
@@ -12,8 +12,12 @@ import milkywayw.utilities.Point;
 public class SnakeRender extends JFrame
 {
     private DrawBoard canvas;
- 
+    
     static final int CELL_SIZE = 20;
+
+    private static final Color BOARD_COLOR = Color.gray;
+    private static final Color SNAKE_COLOR = Color.green;
+    private static final Color FOOD_COLOR = Color.cyan;
     
     public SnakeRender(int width, int height)
     {   
@@ -23,6 +27,7 @@ public class SnakeRender extends JFrame
         add(canvas);
         
         pack();
+        setLocationRelativeTo(null); //center on screen
     }
     
     private void initFrame()
@@ -30,29 +35,16 @@ public class SnakeRender extends JFrame
         getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Snake");
-        setLocationRelativeTo(null);
         setVisible(true);
     }
     
-    public void update(Collection<Point> snake, int width, int height, Point food)
+    public void update(Collection<Point> snake, Point food)
     {
-        canvas.drawBoard();
-        canvas.drawSnake(snake);
-        canvas.drawFood(food);
+        // reset the board, then color in the snake and food
+        canvas.colorBoard(BOARD_COLOR);
+        canvas.colorPoints(snake, SNAKE_COLOR);
+        canvas.colorPoint(food, FOOD_COLOR);
         
         repaint();
-    }
-    
-    public static void main(String[] args)
-    {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                SnakeRender snakeGame = new SnakeRender(5, 6);
-                snakeGame.setVisible(true);
-            }
-        });
     }
 }
